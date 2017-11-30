64248a5abe2ecaa02435567d9ba14eb25e2f6a74
John Paraskevopoulos <io.paraskev@gmail.com>
2017-11-30 18:59:23 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=6.0',
+    'pynacl>=1.2.0',
+]
+
+setup_requirements = [
+]
+
+test_requirements = [
+    'pytest',
+    'tox',
+    'coverage'
+]
+
+setup(
+    name='cenotes_cli',
+    version='0.1.0',
+    description="Cenotes command line application and libraries",
+    long_description=readme + '\n\n' + history,
+    author="John Paraskevopoulos",
+    author_email='ioparaskev@gmail.com',
+    url='https://github.com/ioparaskev/cenotes_cli',
+    packages=find_packages(include=['cenotes_cli']),
+    entry_points={
+        'console_scripts': [
+            'cenotes-cli=cenotes_cli.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    zip_safe=False,
+    keywords='cenotes_cli',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
