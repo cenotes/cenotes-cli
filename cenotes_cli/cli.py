2eb09cb6b2a3d9dc7f7808d60bcfff5ef07da1fb
John Paraskevopoulos <io.paraskev@gmail.com>
2017-11-30 18:57:58 +0200

    Add different option for decryption key and passw
    - Prompts for decryption key and does not allow empty
    - Prompts for encryption password and allows empty

--- cenotes_cli/cli.py
+++ cenotes_cli/cli.py
@@ -15,27 +15,29 @@ from cenotes_cli.cenotes_lib import exceptions, crypto
 @click.option('--text', '-t', prompt="Enter payload or cleartext",
               help="The ciphertext for decryption mode "
                    "or the cleartext for encryption mode")
-@click.option('--key', '-k', default="",
-              prompt="Enter a password to generate the key",
-              help="The key for decryption mode "
-                   "or the password to generate key for encryption mode")
-def main(decrypt, encrypt, text, key):
+@click.option('--password', '-p',
+              help="The password to generate a encryption/decryption key")
+@click.option('--key', '-k',
+              help="The key to decrypt")
+def main(decrypt, encrypt, text, password, key):
     """Console script for cenotes_cli."""
-    if decrypt:
-        key = key or input("Enter your decryption key:")
-        print("Decryption was successful!\n"
-              "Decrypted note: {0}"
-              .format(crypto.decrypt_note(text, key).decode()))
-    elif encrypt:
-        print(key)
-        print("Encryption was successful!\n"
-              "Encrypted payload: {0}\n"
-              "Key to decrypt: {1}"
-              .format(*crypto.encrypt_note(text, key)))
+    try:
+        if decrypt:
+            key = key or click.prompt("Enter your decryption key")
+            print("Decryption was successful!\n"
+                  "Decrypted note: {0}"
+                  .format(crypto.decrypt_note(text, key).decode()))
+        elif encrypt:
+            password = password or click.prompt(
+                "Enter a password to generate the key (press enter to "
+                "autogenerate one)", default="")
+            print("Encryption was successful!\n"
+                  "Encrypted payload: {0}\n"
+                  "Key to decrypt: {1}"
+                  .format(*crypto.encrypt_note(text, password)))
+    except exceptions.CenotesError as err:
+        print("Error: {0}".format(err))
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except exceptions.CenotesError as err:
-        print("Error: {0}".format(err))
+    main()
